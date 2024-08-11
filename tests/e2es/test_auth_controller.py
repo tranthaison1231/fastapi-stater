f55511d06e73388297983515d208c21f87b9c564
Son Tran <son.tran@gmail.com>
2024-08-11 17:31:45 +0700

    feat(master): e2e test

--- tests/e2es/test_auth_controller.py
+++ tests/e2es/test_auth_controller.py
@@ -1,12 +1,16 @@
-def test_login(client):
-    response = client.post(
-        "/api/register",
-        json={
-            "email": "ttson.1714@gmail.com",
-            "password": "test123",
-            "username": "test",
-        },
-    )
-    assert response.status_code == 409
-    response_json = response.json()
-    assert response_json["detail"] == "User with this email already exists"
+class TestAuthController:
+    def setup_class(self):
+        self.url = "/api/register"
+
+    def test_login_fail_with_exist_email(self, client):
+        response = client.post(
+            self.url,
+            json={
+                "email": "ttson.1716@gmail.com",
+                "password": "test123",
+                "username": "test",
+            },
+        )
+        assert response.status_code == 409
+        response_json = response.json()
+        assert response_json["detail"] == "User with this email already exists"
--- tests/e2es/test_post_controller.py
+++ tests/e2es/test_post_controller.py
@@ -1,7 +1,9 @@
-def test_get_posts(client):
-    response = client.get(
-        "/api/posts",
-    )
-    assert response.status_code == 200
-    response_json = response.json()
-    assert response_json == []
+class TestPostController:
+    def setup_class(self):
+        self.url = "/api/posts"
+
+    def test_get_posts_success(self, client):
+        response = client.get(self.url)
+        assert response.status_code == 200
+        response_json = response.json()
+        assert response_json == []
